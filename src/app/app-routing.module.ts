568b399e6ba6409e8929f69e6bbabbb93989401f
Igor Franzon <IgorKiller654@gmail.com>
2024-05-20 13:43:45 -0300

    Atividade app mobile nike

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,8 +8,12 @@ const routes: Routes = [
     pathMatch: 'full'
   },
   {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+    path: 'nike',
+    loadChildren: () => import('./nike/nike.module').then( m => m.NikePageModule)
+  },
+  {
+    path: 'produto',
+    loadChildren: () => import('./produto/produto.module').then( m => m.ProdutoPageModule)
   }
 ];
 
